fb530c7266b989c85c44a9256d8ab61c27f3010b
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-03-12 19:51:28 +0100

    convert deseq2 values from string to float
    Former-commit-id: 9253e56e4143e204623246836a8a9b7a18958dfc

--- dge_visualization/src/utilities/deseq2.js
+++ dge_visualization/src/utilities/deseq2.js
@@ -16,7 +16,9 @@ function parseDeseq2 (content, conditions, deseqResult = new DGE()) {
       name = name.substr(1, name.length - 2)
     }
 
-    deseqResult.addDESeq2Data(name, conditions[0], conditions[1], line[1], line[2], line[3], line[4], line[5], line[6])
+    deseqResult.addDESeq2Data(name, conditions[0], conditions[1], parseFloat(line[1]),
+      parseFloat(line[2]), parseFloat(line[3]), parseFloat(line[4]), parseFloat(line[5]),
+      parseFloat(line[6]))
   }
 
   return deseqResult
