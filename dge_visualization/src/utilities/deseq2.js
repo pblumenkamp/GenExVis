76c8c8aac68f7ac4ddeea65021fba83584f06dd9
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-03-12 19:21:23 +0100

    strip quotes of gene name, if necessary
    Former-commit-id: 1d1baa3e6a5abbff378e593e74e4efc03eee3c5d

--- dge_visualization/src/utilities/deseq2.js
+++ dge_visualization/src/utilities/deseq2.js
@@ -11,7 +11,13 @@ function parseDeseq2 (content, conditions, deseqResult = new DGE()) {
   for (let i = 1, lenLines = content.length; i < lenLines; i++) {
     let line = content[i].split('\t')
 
-    deseqResult.addDESeq2Data(line[0], conditions[0], conditions[1], {
+    let name = line[0]
+    if (name.charAt(0) === '"' && name.charAt(str.length -1) === '"')
+    {
+      name = name.substr(1,str.length -2)
+    }
+
+    deseqResult.addDESeq2Data(name, conditions[0], conditions[1], {
       baseMean: line[1],
       log2FoldChange: line[2],
       lfcSE: line[3],
