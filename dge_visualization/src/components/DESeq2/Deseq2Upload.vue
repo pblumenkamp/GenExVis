f931607bb3a6b455d2cb6c11544fabba48322866
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-03-21 18:31:45 +0100

    add done-icon for finished uploading
    Former-commit-id: fd39407228a042cfca18ad036777af89afee91b5

--- dge_visualization/src/components/DESeq2/Deseq2Upload.vue
+++ dge_visualization/src/components/DESeq2/Deseq2Upload.vue
@@ -13,30 +13,6 @@
       <deseq2-conditions v-if="!(files.length == 0)" :files="files" style="margin-top: 2rem"></deseq2-conditions>
     </div>
   </div>
-
-  <!--<div class="columns" style="margin-top: 1rem">
-    <div class="file has-name column is-two-fifths field is-grouped">
-      <label class="file-label">
-        <input class="file-input" type="file" name="resume" multiple @change="onFileChange">
-        <span class="file-cta">
-          <span class="file-icon">
-            <i class="fa fa-upload"></i>
-          </span>
-          <span class="file-label">
-            Choose a file…
-          </span>
-        </span>
-      </label>
-      <p class="control">
-        <button class="button is-light" @click="reset">Clear</button>
-      </p>
-    </div>
-    <div class="box column is-one-fifth" v-if="!files.length == 0">
-      <ul>
-        <li v-for="file in files">{{ file.name }}</li>
-      </ul>
-    </div>
-  </div>-->
 </template>
 
 <script>
@@ -55,9 +31,7 @@
     },
     methods: {
       onFileChange (e) {
-        console.log(this.files.length)
         for (var i = 0; i < this.files.length; i++) {
-          console.log(i)
           this.conditions.push([])
         }
       }
