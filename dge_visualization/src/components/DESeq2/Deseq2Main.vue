ec664a3ad7b69f6b5b332880c20fa80dbd347320
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-03-28 13:32:00 +0200

    remove link to DESeq2 Upload page
    Former-commit-id: b95b3a3821b48c7d9747c1f7c9b274cb56915b00

--- dge_visualization/src/components/DESeq2/Deseq2Main.vue
+++ dge_visualization/src/components/DESeq2/Deseq2Main.vue
@@ -3,7 +3,6 @@
     <b-row>
       <b-col class="col" cols="1">
         <b-nav vertical class="left_navbar">
-          <b-nav-item to="/deseq2/upload">Upload</b-nav-item>
           <b-nav-item to="/deseq2/overview">Overview</b-nav-item>
           <b-nav-item to="/deseq2/volcano_plot">Volcano Plot</b-nav-item>
         </b-nav>
