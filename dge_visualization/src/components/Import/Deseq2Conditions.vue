1791e70199dbf6c4e3995c0be303dd7d4b44a149
Miriam Müller <miriam.mueller@bio.uni-giessen.de>
2019-06-13 11:26:48 +0200

    added vue multiselect single select for deseq2 analysis condition selection
    Former-commit-id: bf83cff3182ef0cdad609d5506cf59eddbf29ec4

--- dge_visualization/src/components/Import/Deseq2Conditions.vue
+++ dge_visualization/src/components/Import/Deseq2Conditions.vue
@@ -42,34 +42,62 @@
         </span>
       </b-col>
     </b-row>
-    <div style="width: 10rem; margin: 2rem auto 0;">
-      <b-button style="float: left; margin-right: 1rem" @click="parseDataObject">
-        Import files
-      </b-button>
+    <b-row>
+      <b-col>
+        <div style="width: 10rem; margin: 2rem auto 0;">
+          <b-button style="float: left; margin-right: 1rem" @click="parseDataObject">
+            Import files
+          </b-button>
+        </div>
+      </b-col>
+      <div style="width: 100%; margin: 2rem auto 0">
+        <b-col>
+          <h4>Select DESeq2 Type</h4>
+          <multiselect
+            v-model="DESeq2Type"
+            :options="sofa"
+            :multiple="false"
+            :close-on-select="false"
+            :clear-on-select="false"
+            :preserve-search="true"
+            :show-labels="true"
+            :preselect-first="false"
+            selected-label="Selected"
+            select-label="Click to select"
+            deselect-label="Click to remove"
+            placeholder="Choose DESeq2 type"
+            @input="setDeseq2Type"
+          >
+            <template slot="selection" slot-scope="{ values, search, isOpen }">
+              <span v-if="values.length && !isOpen" class="multiselect__single">{{ values.length }} options selected</span>
+            </template>
+          </multiselect>
+        </b-col>
+      </div>
+    </b-row>
+    <font-awesome-icon
+      v-if="importingFiles"
+      :icon="faSpinner"
+      pulse
+      size="2x"
+      class="text-secondary"
+      style="margin-top: 0.1rem"
+    />
+    <font-awesome-icon
+      v-if="importingDone"
+      :icon="faCheckCircle"
+      size="2x"
+      class="text-secondary"
+      style="margin-top: 0.1rem"
+    />
+    <span v-if="importingFailed" v-tooltip.top="{content: importFailedMessage, placement: 'auto', offset: 30}" style="cursor: pointer">
       <font-awesome-icon
-        v-if="importingFiles"
-        :icon="faSpinner"
-        pulse
+        :icon="faTimesCircle"
         size="2x"
         class="text-secondary"
         style="margin-top: 0.1rem"
       />
-      <font-awesome-icon
-        v-if="importingDone"
-        :icon="faCheckCircle"
-        size="2x"
-        class="text-secondary"
-        style="margin-top: 0.1rem"
-      />
-      <span v-if="importingFailed" v-tooltip.top="{content: importFailedMessage, placement: 'auto', offset: 30}" style="cursor: pointer">
-        <font-awesome-icon
-          :icon="faTimesCircle"
-          size="2x"
-          class="text-secondary"
-          style="margin-top: 0.1rem"
-        />
-      </span>
-    </div>
+    </span>
     <b-progress
       v-if="!progress.done"
       :value="progress.counter"
@@ -81,16 +109,20 @@
 </template>
 
 <script>
-  import {STORE_DESEQ2_STATISTICS} from '@/store/action_constants'
-  import {ADD_DESEQ} from '@/store/mutation_constants'
+  import {STORE_DESEQ2_STATISTICS} from '../../store/action_constants'
+  import {ADD_DESEQ} from '../../store/mutation_constants'
 
   import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
   import {faSpinner, faCheckCircle, faTimesCircle, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
 
+  import Multiselect from 'vue-multiselect'
+  import {STORE_DESEQ2Type} from "../../store/action_constants";
+
   export default {
     name: 'Deseq2Conditions',
     components: {
-      FontAwesomeIcon
+      FontAwesomeIcon,
+      Multiselect
     },
     props: {'files': {type: FileList, required: true}},
     data () {
@@ -101,7 +133,12 @@
         importingFailed: false,
         importFailedMessage: 'Import failed',
         parsedDeseq2Data: [],
-        progress: {counter: 0, max: 1000, done: true}
+        progress: {counter: 0, max: 1000, done: true},
+        // multiselect Miri
+        // will contain selected item
+        DESeq2Type: " ",
+        // options to select from
+        sofa: ['antisense primary transcript','antisense RNA','ARS','assembly component','attenuator','autocatalytically spliced intron','binding site','branch site','C D box snoRNA','cap','cDNA match','CDS','centromere','chromosomal structural element','chromosome','chromosome band','clip','clone','clone insert end','clone insert start','coding conserved region','coding exon','codon','conserved region','contig','CpG island','cross genome match','decayed exon','deletion','deletion junction','direct repeat','dispersed repeat','ds oligo','enhancer','enzymatic RNA','EST','EST match','exon','exon junction','experimental result region','expressed sequence match','five prime cis splice site','five prime coding exon','five prime coding exon coding region','five prime coding exon noncoding region','five prime exon coding region','five prime splice site','five prime UTR','flanking region','gene','gene part','golden path','golden path fragment','group I intron','group II intron','guide RNA','hammerhead ribozyme','insertion','insertion site','insulator','intergenic region','interior coding exon','intron','inverted repeat','junction','large subunit rRNA','match','match part','match set','mature protein region','mature transcript','methylated A','methylated base feature','methylated C','microsatellite','minisatellite','miRNA','modified base','modified base site','mRNA','nc conserved region','nc primary transcript','ncRNA','non transcribed region','noncoding exon','nuclease binding site','nuclease sensitive site','nucleotide match','nucleotide motif','oligo','operator','operon','ORF','origin of replication','PCR product','polyA sequence','polyA signal sequence','polyA site','polypeptide','polypyrimidine tract','possible assembly error','possible base call error','primary transcript','primer','promoter','protein binding site','protein coding primary transcript','protein match','proviral region','pseudogene','pseudogenic region','rasiRNA','read','read pair','reading frame','reagent','region','remark','repeat family','repeat region','restriction fragment','RFLP fragment','ribosome entry site','ribozyme','RNA motif','RNAi reagent','RNase MRP RNA','RNase P RNA','rRNA','rRNA 18S','rRNA 28S','rRNA 5','rRNA 5 8S','rRNA 5S','rRNA large subunit primary transcript','rRNA primary transcript','SAGE tag','satellite DNA','scRNA','sequence assembly','sequence difference','sequence feature','Sequence Ontology','sequence variant obs','signal peptide','silencer','siRNA','small regulatory ncRNA','small subunit rRNA','snoRNA','SNP','snRNA','splice enhancer','splice site','spliceosomal intron','SRP RNA','ss oligo','start codon','stop codon','stRNA','STS','supercontig','tag','tandem repeat','telomerase RNA','telomere','terminator','TF binding site','three prime cis splice site','three prime coding exon coding region','three prime coding exon noncoding region','three prime exon coding region','three prime splice site','three prime UTR','tiling path','tiling path fragment','trans splice acceptor site','transcribed region','transcript','transcription end site','translated nucleotide match','transposable element','transposable element insertion site','tRNA','TSS','U1 snRNA','U11 snRNA','U12 snRNA','U14 snoRNA','U2 snRNA','U4 snRNA','U4atac snRNA','U5 snRNA','U6 snRNA','U6atac snRNA','ultracontig','UTR','vault RNA','virtual sequence','Y RNA']
       }
     },
     computed: {
@@ -135,33 +172,33 @@
       files: {
         immediate: true,
         handler (newfiles) {
-          let dataObject = []
-          let index = 0
+          let dataObject = [];
+          let index = 0;
           for (var file of newfiles) {
-            let sugglist = this.suggestregex(file.name)
-            dataObject.push({file: file, conditions: sugglist, index: index, validEntry: null, errorText: ''})
+            let sugglist = this.suggestregex(file.name);
+            dataObject.push({file: file, conditions: sugglist, index: index, validEntry: null, errorText: ''});
             index++
           }
-          this.dataObject = dataObject
+          this.dataObject = dataObject;
           return dataObject
         }
       }
     },
     methods: {
       parseDataObject () {
-        let vue = this
+        let vue = this;
         if (!vue.validate(vue.dataObject)) {
-          vue.importingFailed = true
+          vue.importingFailed = true;
           return
         }
-        vue.importingDone = false
-        vue.importingFailed = false
-        vue.importingFiles = true
-        vue.progress.done = false
-        let promises = []
-        let files = []
+        vue.importingDone = false;
+        vue.importingFailed = false;
+        vue.importingFiles = true;
+        vue.progress.done = false;
+        let promises = [];
+        let files = [];
         for (let {file, conditions} of this.dataObject) {
-          promises.push(this.readDeseq2FileAsText(file, conditions))
+          promises.push(this.readDeseq2FileAsText(file, conditions));
           files.push(file.name)
         }
 
@@ -173,40 +210,40 @@
           .then(values => {
             vue.$store.dispatch(STORE_DESEQ2_STATISTICS, {deseq2Contents: values, progress: vue.progress})
               .then(() => {
-                vue.importingFiles = false
+                vue.importingFiles = false;
                 vue.importingDone = true
               }, reason => {
-                vue.importingFailedMessage = reason
-                vue.importingFiles = false
+                vue.importingFailedMessage = reason;
+                vue.importingFiles = false;
                 vue.importingFailed = true
             })
           })
       },
       validate (data) {
-        let valid = true
+        let valid = true;
         for (let i = 0; i < data.length - 1; i++) {
-          let entry = data[i]
+          let entry = data[i];
           if (entry.conditions[0] === entry.conditions[1]) {
-            valid = false
-            entry.validEntry = false
+            valid = false;
+            entry.validEntry = false;
             data[i].errorText = 'Conditions must differ in a DESeq2 comparison'
           } else {
             entry.validEntry = null
           }
         }
         for (let i = 0; i < data.length - 1; i++) {
-          let entryA = data[i]
-          let sameConds = []
+          let entryA = data[i];
+          let sameConds = [];
           for (let j = i + 1; j < data.length; j++) {
-            let entryB = data[j]
+            let entryB = data[j];
             if (entryB.validEntry !== false) {
               if (entryA.conditions[0] === entryB.conditions[0] && entryA.conditions[1] === entryB.conditions[1]) {
                 if (entryA.validEntry !== false) {
-                  entryA.validEntry = false
-                  valid = false
+                  entryA.validEntry = false;
+                  valid = false;
                   sameConds.push(i)
                 }
-                entryB.validEntry = false
+                entryB.validEntry = false;
                 sameConds.push(j)
               }
             }
@@ -218,42 +255,42 @@
         return valid
       },
       readDeseq2FileAsText (file, conditions) {
-        const reader = new FileReader()
+        const reader = new FileReader();
 
         return new Promise((resolve, reject) => {
           reader.onerror = () => {
-            reader.abort()
+            reader.abort();
             reject(new DOMException('Problem parsing input file.'))
-          }
+          };
 
           reader.onload = () => {
             resolve({content: reader.result, conditions: conditions})
-          }
+          };
           reader.readAsText(file)
         })
       },
       suggestregex (filename) {
-        let suggestionlist = ['', '']
-        let tempdict = {}
+        let suggestionlist = ['', ''];
+        let tempdict = {};
         // versus check block
-        let vsbool = false
-        let vsindex = 0
-        let vsregex = RegExp('[^a-z0-9]vs[^a-z0-9]', 'i')
-        let vsmatch = vsregex.exec(filename)
+        let vsbool = false;
+        let vsindex = 0;
+        let vsregex = RegExp('[^a-z0-9]vs[^a-z0-9]', 'i');
+        let vsmatch = vsregex.exec(filename);
         if (vsmatch !== null) {
-          vsbool = true
+          vsbool = true;
           vsindex = vsmatch.index
         }
         // versus check block END
         for (let entry of this.$store.state.registeredConditions) {
-          let regex = RegExp('[^a-z0-9]*' + entry.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), 'i')
-          let match = regex.exec(filename)
+          let regex = RegExp('[^a-z0-9]*' + entry.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), 'i');
+          let match = regex.exec(filename);
           if (match !== null) {
             tempdict[match.index] = entry
           }
         }
-        let keyarray = Object.keys(tempdict)
-        let keylength = Object.keys(tempdict).length
+        let keyarray = Object.keys(tempdict);
+        let keylength = Object.keys(tempdict).length;
         if (keylength === 2) {
           for (let i = 0; i <= 1; i++) {
             suggestionlist[i] = tempdict[keyarray[i]]
@@ -269,12 +306,20 @@
           // (now) pass
         }
         return suggestionlist
+      },
+      // setting the Deseq2Type whenever the selection changes
+      setDeseq2Type () {
+        this.$store.dispatch(STORE_DESEQ2Type, this.DESeq2Type);
+        // eslint-disable-next-line no-console
+        console.log(this.$store.state.deseq2Type);
+        // eslint-disable-next-line no-console
+        console.log(typeof (this.$store.state.deseq2Type));
       }
     }
   }
 </script>
 
-<style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css">
   .tooltip {
     display: block !important;
     z-index: 10000;
