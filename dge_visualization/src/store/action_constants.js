c30df13bcde5c38d71b528b2d53727266eb903c5
Miriam Müller <miriam.mueller@bio.uni-giessen.de>
2019-06-17 14:23:38 +0200

    fixed ESLint errors
    Former-commit-id: 413f03abd29defbf3d7765d51981d0400e4bdb66

--- dge_visualization/src/store/action_constants.js
+++ dge_visualization/src/store/action_constants.js
@@ -1,8 +1,8 @@
-export const STORE_DESEQ2_STATISTICS = 'store_deseq2_statistics'
-export const EXTEND_FILE_LIST = 'extend_file_list'
-export const REGISTER_CONDITION = 'register_condition'
-export const SEARCH_REGEX = 'search_regex'
-export const STORE_COUNT_TABLE = 'store_count_table'
-export const SET_SUBDGE = 'set_subDGE'
-export const STORE_GFF3_DATA = 'store_gff3_data'
-export const STORE_DESEQ2Type = 'store_deseq2Type'
+export const STORE_DESEQ2_STATISTICS = 'store_deseq2_statistics';
+export const EXTEND_FILE_LIST = 'extend_file_list';
+export const REGISTER_CONDITION = 'register_condition';
+export const SEARCH_REGEX = 'search_regex';
+export const STORE_COUNT_TABLE = 'store_count_table';
+export const SET_SUBDGE = 'set_subDGE';
+export const STORE_GFF3_DATA = 'store_gff3_data';
+export const STORE_DESEQ2Type = 'store_deseq2Type';
