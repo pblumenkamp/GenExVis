02cec547e689021372246b55b010a1ba0898bc43
Miriam Müller <miriam.mueller@bio.uni-giessen.de>
2019-07-26 12:18:01 +0200

    touched but unchanged
    Former-commit-id: 815fb710a998aabc2c8d61e0364aaff4d7d35fae

--- dge_visualization/src/utilities/deseq2.js
+++ dge_visualization/src/utilities/deseq2.js
@@ -1,8 +1,8 @@
 import {DGE} from './dge'
 
 function parseDeseq2 (content, conditions, deseqResult = new DGE()) {
-  content = content.split('\n')
-  let header = content[0].split('\t')
+  content = content.split('\n');
+  let header = content[0].split('\t');
 
   if (header.length !== 7) {
     throw new Error('Incorrect header in DESeq2 file found. File should contain 7 columns.')
@@ -13,9 +13,11 @@ function parseDeseq2 (content, conditions, deseqResult = new DGE()) {
       continue
     }
 
-    let line = content[i].split('\t')
-
-    let name = line[0].replace(/['"]+/g, '')
+    let line = content[i].split('\t');
+    // Miri's IDEE, falls DESeq2 doch die .gene Konvention hat //
+    //let nameArray=line[0].split('.');
+    //let name=nameArray[0];
+    let name = line[0].replace(/['"]+/g, '');
 
     deseqResult.addDESeq2Data(name, conditions[0], conditions[1], parseFloat(line[1]),
       parseFloat(line[2]), parseFloat(line[3]), parseFloat(line[4]), parseFloat(line[5]),
