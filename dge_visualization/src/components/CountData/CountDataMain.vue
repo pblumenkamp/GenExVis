65879ec6f363dd5acc2fba2554da935cd790b4d0
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2019-08-16 16:49:40 +0200

    remove old 'imported data' overview
    Former-commit-id: 4be78668aa4214ae7309f8ccea44a295196e558a

--- dge_visualization/src/components/CountData/CountDataMain.vue
+++ dge_visualization/src/components/CountData/CountDataMain.vue
@@ -1,11 +1,5 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col cols="10">
-        <router-view />
-      </b-col>
-    </b-row>
-  </b-container>
+  <router-view />
 </template>
 
 <script>
@@ -13,34 +7,3 @@
     name: 'CountData'
   }
 </script>
-
-<style scoped>
-  .filesBox {
-    text-align: left;
-    overflow-y: scroll;
-    overflow-x: scroll;
-    height: auto;
-    max-height: 25rem;
-    white-space: nowrap;
-    padding: 0.5rem
-  }
-  #style-1::-webkit-scrollbar-track
-  {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-    border-radius: 10px;
-    background-color: #F5F5F5;
-  }
-
-  #style-1::-webkit-scrollbar
-  {
-    width: 12px;
-    background-color: #F5F5F5;
-  }
-
-  #style-1::-webkit-scrollbar-thumb
-  {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-    background-color: #555;
-  }
-</style>
